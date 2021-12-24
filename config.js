1ff6e91beec1233901f418b7414898b442d8f125
huyphamne <96606727+huyphamne@users.noreply.github.com>
2021-12-24 12:06:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,13 @@ const CONFIG = {
     introTitle: 'Changg<33',
     
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng huyy đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng huyy đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'tối nayy điii noenn với huyy nkaa🥰',
+   
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'tối nayy điii chơi noenn với huyy nkaa <3',
-    btnReply: 'Changg<3',
+    question: 'nhân tiện đây changg có người yêu chưa<3',
+    btnReply: 'nếu là chưa thì cho huy mụt cơ hội nka<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
